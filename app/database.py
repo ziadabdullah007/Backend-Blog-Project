57dc4a8e0de54223d928d3ae8c4a29f0dcd9363a
ziadabdullah007 <zyadabdallah11@gmail.com>
2026-04-23 13:33:03 +0200

    make connect with MSSQL and update database

--- app/database.py
+++ app/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost/blog_db"
+DATABASE_URL = "mssql+pyodbc://@localhost/blog_db?driver=ODBC+Driver+17+for+SQL+Server"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from routes import user_routes, post_routes, comment_routes
+
+from app.database import Base, engine
+from models import user_model, post_model, comment_model
+
+app = FastAPI()
+
+app.include_router(user_routes.router)
+app.include_router(post_routes.router)
+app.include_router(comment_routes.router)
+
+@app.get("/")
+def root():
+    return {"message": "Blog API is running"}
