71ae61fc8e97d53ea634a8c2ddba70127d45f222
ahmedwalidahmad-debug <ahmedwalidahmad@gmail.com>
2026-04-30 14:14:02 +0300

    same

--- routes/post_routes.py
+++ routes/post_routes.py
@@ -3,9 +3,6 @@ from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from models.post_model import Post
 from schemas.post_schema import PostCreate
-from app.redis_client import redis_client
-from app.logger import logger
-import json
 
 router = APIRouter(prefix="/posts", tags=["Posts"])
 
@@ -16,11 +13,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-def clear_posts_cache():
-    for key in redis_client.scan_iter("posts:*"):
-        redis_client.delete(key)
 
 
 @router.post("/")
@@ -34,9 +26,6 @@ def create_post(post: PostCreate, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-
-    clear_posts_cache()
-    logger.info(f"POST /posts created post id={new_post.id}")
 
     return {
         "id": new_post.id,
@@ -52,13 +41,6 @@ def get_posts(
     limit: int = Query(10, ge=1, le=100),
     db: Session = Depends(get_db)
 ):
-    cache_key = f"posts:page={page}:limit={limit}"
-
-    cached_posts = redis_client.get(cache_key)
-    if cached_posts:
-        logger.info(f"GET /posts?page={page}&limit={limit} from Redis cache")
-        return json.loads(cached_posts)
-
     skip = (page - 1) * limit
 
     posts = db.query(Post).order_by(Post.id).offset(skip).limit(limit).all()
@@ -74,27 +56,19 @@ def get_posts(
             "user_id": post.user_id
         })
 
-    response = {
+    return {
         "page": page,
         "limit": limit,
         "total": total,
         "data": posts_data
     }
 
-    redis_client.setex(cache_key, 60, json.dumps(response))
-    logger.info(f"GET /posts?page={page}&limit={limit} from database")
-
-    return response
-
 
 @router.get("/{post_id}")
 def get_post(post_id: int, db: Session = Depends(get_db)):
-    logger.info(f"GET /posts/{post_id}")
-
     post = db.query(Post).filter(Post.id == post_id).first()
 
     if post is None:
-        logger.warning(f"GET /posts/{post_id} - Post not found")
         raise HTTPException(status_code=404, detail="Post not found")
 
     return {
@@ -107,18 +81,12 @@ def get_post(post_id: int, db: Session = Depends(get_db)):
 
 @router.delete("/{post_id}")
 def delete_post(post_id: int, db: Session = Depends(get_db)):
-    logger.info(f"DELETE /posts/{post_id}")
-
     post = db.query(Post).filter(Post.id == post_id).first()
 
     if post is None:
-        logger.warning(f"DELETE /posts/{post_id} - Post not found")
         raise HTTPException(status_code=404, detail="Post not found")
 
     db.delete(post)
     db.commit()
-
-    clear_posts_cache()
-    logger.info(f"DELETE /posts/{post_id} success")
 
     return {"message": "Post deleted"}
