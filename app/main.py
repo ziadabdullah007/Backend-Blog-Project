af1cdb65cc3020b907ca8eee7f550d3782f85301
qhmedkhaled10 <midoahmedkhaled2004@gmail.com>
2026-04-30 16:14:59 +0300

    Add monitoring with Prometheus

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
 from routes import user_routes, post_routes, comment_routes
 from app.database import Base, engine
 from models import user_model, post_model, comment_model
@@ -7,9 +9,12 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+Instrumentator().instrument(app).expose(app)
+
 app.include_router(user_routes.router)
 app.include_router(post_routes.router)
 app.include_router(comment_routes.router)
+
 
 @app.get("/")
 def root():
