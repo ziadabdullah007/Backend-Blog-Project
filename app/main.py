5972d4ff01a15d8caff6839212535d029d5672ab
sandyhany <153460349+SandySadek@users.noreply.github.com>
2026-05-10 22:55:45 +0300

    app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes import user_routes, post_routes, comment_routes
 from app.database import Base, engine
 from models import user_model, post_model, comment_model
@@ -9,14 +8,18 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-Instrumentator().instrument(app).expose(app)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_routes.router)
 app.include_router(post_routes.router)
 app.include_router(comment_routes.router)
 
-
 @app.get("/")
 def root():
     return {"message": "Blog API is running"}
-
